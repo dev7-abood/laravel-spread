0b4cf02e9574b625404b897291bef589dd355092
Abood <dev7.abood@gmail.com>
2022-09-12 01:53:07 +0300

    improve

--- resources/views/home/mail/send-business-mail.blade.php
+++ resources/views/home/mail/send-business-mail.blade.php
@@ -2,27 +2,27 @@
 
 <div style="display: flex; justify-content: space-between">
     <p>{{$data->email}}</p>
-    <p>:{{__('mail.Sender')}}</p>
+    <p>{{__('mail.Sender')}}</p>
 </div>
 
 <div style="display: flex; justify-content: space-between">
     <p>{{$data->name}}</p>
-    <p>:{{__('mail.The name')}}</p>
+    <p>{{__('mail.The name')}}</p>
 </div>
 
 <div style="display: flex; justify-content: space-between">
     <p>{{$data->telephone}}</p>
-    <p>:{{__('mail.Phone number')}}</p>
+    <p>{{__('mail.Phone number')}}</p>
 </div>
 
 <div style="display: flex; justify-content: space-between">
     <p>{{$data->comp_name}}</p>
-    <p>:{{__('mail.Company name')}}</p>
+    <p>{{__('mail.Company name')}}</p>
 </div>
 
 <div style="display: flex; justify-content: space-between">
     <p>{{$data->target_segment}}</p>
-    <p>:{{__('mail.Target segment')}}</p>
+    <p>{{__('mail.Target segment')}}</p>
 </div>
 
 @endcomponent
