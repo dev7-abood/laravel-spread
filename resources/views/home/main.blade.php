b3a3e9a1a80f67d195bb8219dbde7befebda1565
Abood <dev7.abood@gmail.com>
2022-09-12 01:56:47 +0300

    add google tags to home page

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -22,6 +22,12 @@
     <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
     <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="/assets/css/style.css" rel="stylesheet">
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WJVHMN8');</script>
+
     @livewireStyles
 </head>
 
@@ -378,6 +384,14 @@
 
 <!-- Main JS File -->
 <script src="/assets/js/main.js"></script>
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WJVHMN8');</script>
+
+
 @livewireScripts
 
 <script>
